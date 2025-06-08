71ff66c8942a0460bc7f4b4f72ab661a0bda201f
ReemWSW <widsawa.d31@gmail.com>
2025-06-08 16:31:36 +0700

    Update Gradle and Firebase configurations; add Firebase dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    // firebase
+    id("com.google.gms.google-services") 
 }
 
 android {
     namespace = "com.example.e_hong_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+    ndkVersion = "27.0.12077973" 
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
